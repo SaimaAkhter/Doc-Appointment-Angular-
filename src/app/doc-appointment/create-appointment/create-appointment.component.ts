96d3d44886b43a5ac6d0c96ddcf6989e307ce520
Saima_TL <saima@trucklagbe.com>
2022-08-28 01:11:52 +0600

    fixed bugs

--- src/app/doc-appointment/create-appointment/create-appointment.component.ts
+++ src/app/doc-appointment/create-appointment/create-appointment.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup , FormControl } from '@angular/forms';
+import { FormGroup , FormControl , Validators } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-create-appointment',
@@ -8,22 +9,27 @@ import { FormGroup , FormControl } from '@angular/forms';
 })
 export class CreateAppointmentComponent implements OnInit {
 
-  contactForm = new FormGroup({
-    firstname: new FormControl(),
-    lastname: new FormControl(),
-    email: new FormControl(),
-    gender: new FormControl(),
-    age: new FormControl(),
-    date: new FormControl(),
-    time: new FormControl()
-  })
+  appointmentForm = new FormGroup({
+    firstname: new FormControl('',[Validators.required,
+      Validators.maxLength(40)]),
+    lastname: new FormControl('',[Validators.required, Validators.maxLength(40)]),
+    email: new FormControl('',[Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")]),
+    gender: new FormControl('',[Validators.required]),
+    age: new FormControl('',[Validators.required]),
+    date: new FormControl('',[Validators.required]),
+    time: new FormControl('' ,[Validators.required])
+  }) ;
  
  
   onSubmit() {
-    console.log(this.contactForm.value);
+    console.log(this.appointmentForm.value);
+    localStorage.setItem('appointmentForm',this.appointmentForm.value) ;
   }
-  constructor() { }
 
+  cancel() {
+    this.modalService.dismissAll() ;
+  }
+  constructor(private modalService: NgbModal) { }
   ngOnInit() {
   }
 
--- src/app/doc-appointment/doc-appointment-routing.module.ts
+++ src/app/doc-appointment/doc-appointment-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
         component: DocAppointmentComponent
     },
     {
-      path: ':id/details',
+      path: ':id',
       component: DocAppointmentComponent
     }
 ];
